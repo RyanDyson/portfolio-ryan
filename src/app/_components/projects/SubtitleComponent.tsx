ce03d2fa8a958462bd1174edc23b4d54cb0dcfb8
Ryan Dyson Darmawan <dysonryan6@gmail.com>
2024-09-06 23:55:50 +0800

    fukc you vercel

--- src/app/_components/projects/SubtitleComponent.tsx
+++ src/app/_components/projects/SubtitleComponent.tsx
@@ -2,28 +2,40 @@ import { CommonText } from "../text/CommonText";
 
 export const SubtitleComponent = (subtitle: string) => {
   return [
-    <div className="leading-[9vw] text-[9vw] font-extrabold w-full flex justify-between">
+    <div
+      key={0}
+      className="leading-[9vw] text-[9vw] font-extrabold w-full flex justify-between"
+    >
       {subtitle.split("").map((item, index) => (
         <div key={index}>
           <CommonText key={index}>{item}</CommonText>
         </div>
       ))}
     </div>,
-    <div className="leading-[9vw] text-[9vw] font-extrabold w-full flex justify-between">
+    <div
+      key={1}
+      className="leading-[9vw] text-[9vw] font-extrabold w-full flex justify-between"
+    >
       {subtitle.split("").map((item, index) => (
         <div key={index}>
           <CommonText key={index}>{item}</CommonText>
         </div>
       ))}
     </div>,
-    <div className="leading-[9vw] text-[9vw] font-extrabold w-full flex justify-between">
+    <div
+      key={2}
+      className="leading-[9vw] text-[9vw] font-extrabold w-full flex justify-between"
+    >
       {subtitle.split("").map((item, index) => (
         <div key={index}>
           <CommonText key={index}>{item}</CommonText>
         </div>
       ))}
     </div>,
-    <div className="leading-[9vw] text-[9vw] font-extrabold w-full flex justify-between">
+    <div
+      key={3}
+      className="leading-[9vw] text-[9vw] font-extrabold w-full flex justify-between"
+    >
       {subtitle.split("").map((item, index) => (
         <div key={index}>
           <CommonText key={index}>{item}</CommonText>
--- src/app/_components/projects/TitleComponent.tsx
+++ src/app/_components/projects/TitleComponent.tsx
@@ -1,28 +1,40 @@
 import { DisplayText } from "../text/DisplayText";
 export const TitleComponent = (title: string) => {
   return [
-    <div className="leading-[11vw] text-[11vw] w-full flex justify-between">
+    <div
+      key={0}
+      className="leading-[11vw] text-[11vw] w-full flex justify-between"
+    >
       {title.split("").map((item, index) => (
         <div key={index}>
           <DisplayText key={index}>{item}</DisplayText>
         </div>
       ))}
     </div>,
-    <div className="leading-[11vw] text-[11vw] w-full flex justify-between">
+    <div
+      key={1}
+      className="leading-[11vw] text-[11vw] w-full flex justify-between"
+    >
       {title.split("").map((item, index) => (
         <div key={index}>
           <DisplayText key={index}>{item}</DisplayText>
         </div>
       ))}
     </div>,
-    <div className="leading-[11vw] text-[11vw] w-full flex justify-between">
+    <div
+      key={2}
+      className="leading-[11vw] text-[11vw] w-full flex justify-between"
+    >
       {title.split("").map((item, index) => (
         <div key={index}>
           <DisplayText key={index}>{item}</DisplayText>
         </div>
       ))}
     </div>,
-    <div className="leading-[11vw] text-[11vw] w-full flex justify-between">
+    <div
+      key={3}
+      className="leading-[11vw] text-[11vw] w-full flex justify-between"
+    >
       {title.split("").map((item, index) => (
         <div key={index}>
           <DisplayText key={index}>{item}</DisplayText>
