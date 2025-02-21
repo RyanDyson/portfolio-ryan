97990dd3d3142043be6d856e8e9d84f368fce164
Ryan Dyson Darmawan <dysonryan6@gmail.com>
2025-02-21 13:35:17 +0800

    Added vercel analytics

--- src/app/_components/Navigation.tsx
+++ src/app/_components/Navigation.tsx
@@ -57,7 +57,7 @@ export function Navigation({ isHomeInView }: NavigationProps) {
         >
           <motion.div className="relative flex justify-center items-center backdrop-blur-xl z-50 opacity-100">
             <motion.div
-              className="flex relative justify-around items-center bg-slate-900/50 border border-slate-400/60 text-amber-50 backdrop-blur-xl overflow-hidden w-12 md:w-16 h-12 md:h-10 transition-all rounded-full"
+              className="flex relative justify-around items-center bg-slate-900/90 border border-slate-400/60 text-amber-50 backdrop-blur-xl overflow-hidden w-12 md:w-16 h-12 md:h-10 transition-all rounded-full"
               animate={controls}
               transition={{ duration: 0.2, ease: "easeInOut" }}
               onMouseEnter={() => setIsHovered(true)}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ryan's Portfolio",
+  icons: "./favicon.ico",
 };
 
 export default function RootLayout({
@@ -19,6 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className}`}>
         <SpeedInsights />
         {children}
+        <Analytics />
       </body>
     </html>
   );
